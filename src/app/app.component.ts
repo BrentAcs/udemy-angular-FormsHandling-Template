107d26057c7ce88c3580558a57530b6b02a7e692
Brent Acs <brent.mann@gmail.com>
2021-02-18 13:17:58 -0500

    EoL 15.198 added form.setValue to set the entire contents of the form. The added form.patchValue to set a specific input element.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,23 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = "Superuser";
+
+    // example setting every value on the form, not always the best approach.
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: "",
+    //   },
+    //   secret: "teacher",
+    //   questionAnswer: "",
+    //   gender: "female",
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      }
+    });
   }
 
   // onSubmit(form: HTMLFormElement) {
